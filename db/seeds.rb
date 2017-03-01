f2af492c98f07cc7fe906c2cf4c4f9d6b739cbdb
calozf <calozf@gmail.com>
2017-03-01 11:39:25 +0100

    populated seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(:name => "seed-generated product", :description => "I also created this product without using HTML form.")
+Product.create(:name => "White Bike", :description => "Awesome white bike. Perfect for a summer adventure in the city.", :image_url => "https://s-media-cache-ak0.pinimg.com/originals/3e/39/86/3e398698f9ffc228edeeb40c325e6c3d.jpg", :price => 629.0, :colour => "White")
+
+Product.create(:name => "Black Bike", :description => "Great black bike. Awesome for biking in the forest.", :image_url => "https://www.rutlandcycling.com/images/marin-fairfax-sc2-ig-2016-hybrid-bike-black.jpg?maxheight=1200", :price => 529.99, :colour => "Black")
+
+Product.create(:name => "Blue Bike", :description => "Great bike.", :image_url => "http://bike.whistlerblackcomb.com/~/media/Bike-Park/Orientation-Guide/2016/1920OrientaionGuideDH.ashx?la=en&hash=D5513672CA9F92C9480B1A670728CA0DCA371167", :price => 325.99, :colour => "Blue")
+
+Product.create(:name => "Purple Bike", :description => "Purple bike.", :image_url => "http://salsacycles.com/files/bikes/BEARGREASE_CARBON_X7_Bike-Carousel_1.jpg", :price => 459.99, :colour => "Purple")
