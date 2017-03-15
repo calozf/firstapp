ecc52ad9d9ebaabc7ec7f1dcb9db1852043461d9
calozf <calozf@gmail.com>
2017-03-15 23:21:12 +0100

    5.7 added comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
     if Rails.env.development?
@@ -9,4 +10,12 @@ class Product < ApplicationRecord
     end 
   end
 
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+  
 end
