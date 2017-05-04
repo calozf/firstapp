a6c7d47b1f0976b9bd0dd0ec553f3ba6cad3cb19
calozf <calozf@gmail.com>
2017-05-04 15:12:28 +0200

    fixed payment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,6 @@ class CommentsController < ApplicationController
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
       respond_to do |format|
-        byebug
         if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
